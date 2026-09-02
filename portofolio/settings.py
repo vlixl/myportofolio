980dd39759aeb5fca086ac4d61bcea2156509204
Leow Vincent Vintizel <vlixl.tech@gmail.com>
2026-09-02 15:40:14 +0700

    Updated PWS Deployment URL

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -16,7 +16,7 @@ import os
 from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "<your PWS deployment URL>"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "leow-vincent-myportofolio.pws.cs.ui.ac.id"]
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
