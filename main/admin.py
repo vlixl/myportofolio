9da0d8bab3abfc095de685b2caad39ef9769645b
Vlixl <vlixl.tech@gmail.com>
2026-09-26 16:04:39 +0700

    feat: register models in admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Experience, Music, Education, Achievement, Photo, Project
 
 # Register your models here.
+admin.site.register(Experience)
+admin.site.register(Music)
+admin.site.register(Education)
+admin.site.register(Achievement)
+admin.site.register(Photo)
+admin.site.register(Project)
